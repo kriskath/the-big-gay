7b0c09c9152751aa075ef419b581b9c05fd65a55
Kris <Kristopher.Kath@gmail.com>
2023-10-15 19:44:01 -0700

    import underclone files

---
+++ Assets/Underclone-Files/Scripts/Player.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    private int health = 10;
+    private Rigidbody2D rb;
+    private SpriteRenderer sprite;
+    private float vertical;
+    private float horizontal;
+    public bool canMove = true;
+    private Animator animator;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        sprite = GetComponent<SpriteRenderer>();
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        if (canMove) {
+            vertical = Input.GetAxisRaw("Vertical");
+            horizontal = Input.GetAxisRaw("Horizontal");
+            switch ((int) vertical) {
+                case (-1):
+                    animator.SetBool("WalkDown", true);
+                    animator.SetBool("WalkUp", false);
+                    break;
+                case (1):
+                    animator.SetBool("WalkUp", true);
+                    animator.SetBool("WalkDown", false);
+                    break;
+                default:
+                    animator.SetBool("WalkDown", false);
+                    animator.SetBool("WalkUp", false);
+                    break;
+            }
+
+            switch ((int)horizontal) {
+                case (-1):
+                    animator.SetBool("WalkLeft", true);
+                    animator.SetBool("WalkRight", false);
+                    break;
+                case (1):
+                    animator.SetBool("WalkRight", true);
+                    animator.SetBool("WalkLeft", false);
+                    break;
+                default:
+                    animator.SetBool("WalkLeft", false);
+                    animator.SetBool("WalkRight", false);
+                    break;
+            }
+        }
+
+        rb.velocity = new Vector2(horizontal, vertical);
+
+        if (Input.GetKeyDown("e")) {
+            Interact();
+        }
+    }
+
+    public void StopMoving()
+    {
+        this.horizontal = 0;
+        this.vertical = 0;
+        this.canMove = false;
+    }
+
+    void Interact()
+    {
+
+    }
+}
---
+++ Assets/Underclone-Files/Scripts/TextWritter.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using TMPro;
+using System.Text.RegularExpressions;
+
+public class TextWritter : MonoBehaviour
+{
+
+    private float delay = .05f;
+    private string text;
+    private string currentText = "";
+    private AudioSource voice;
+    private Regex regex;
+
+    // Use this for initialization
+    void Start()
+    {
+        this.regex = new Regex("^[a-zA-Z0-9]*$");
+        this.voice = GetComponent<AudioSource>();
+        this.text = GetComponent<TextMeshPro>().text;
+        StartCoroutine(ShowText());
+    }
+
+    void OnEnable()
+    {
+        StartCoroutine(ShowText());
+    }
+
+    void OnDisable()
+    {
+        currentText = "";
+    }
+
+    IEnumerator ShowText()
+    {
+        for (int i = 0; i < text.Length; i++) {
+            string currentLetter = text[i].ToString();
+            this.SpeakText(currentLetter);
+            this.GetComponent<TextMeshPro>().text = currentText += currentLetter;
+            yield return new WaitForSeconds(delay);
+        }
+    }
+
+    void SpeakText(string currentLetter)
+    {
+        if (this.voice && regex.IsMatch(currentLetter)) {
+            this.voice.Play();
+        }
+    }
+}
